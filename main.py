5ef3771a4d51fab082b28c2bbf182b7299a9abfa
Jason Ma <Jason Ma@deep-cardinal>
2011-05-30 00:06:25 -0700

    New font families.

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class index:
   <body bgcolor="#FFF"> 
     <div id="loading">
       <center><img src="../static/img/loading2.png" height=400px/></center> <br/> <br/>
-      <div id="status" style="text-align: center; margin-top: 70px; margin-left: auto; margin-right: auto; font-family: helvetica, sans-serif;">Welcome.</div>
+      <div id="status" style="text-align: center; margin-top: 70px; margin-left: auto; margin-right: auto; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Welcome.</div>
     </div>
     <div id="chart" style="display: none;">
       <div id="buttons" style="margin-left: auto; margin-right: auto; margin-top: 20px; text-align: center;">
@@ -49,7 +49,7 @@ class index:
       <div id="chart-container-1" style="width: 100%; height: 500px;"></div>
       <div id="logo" style="position: absolute; top: 10px; left: 10px;"><img src="../static/img/logo.png" height="72px"/></div>
       <div id="show-no-items" style="position: absolute; top: 80px; left: 0px; width: 100%;">
-        <div id="no-items-msg" style="margin-top: 200px; text-align: center; font-size: 150%; font-family: Helvetica, sans-serif;">Click on a disease scheme, above.</div>
+        <div id="no-items-msg" style="margin-top: 200px; text-align: center; font-size: 150%; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Click on a disease scheme, above.</div>
       </div>
       <div id="export-button" style="display: none; position: absolute; top: 20px; right: 30px;">
         <img src="../static/img/print_button.png" height=30px onClick="exportNotes()"/></div>
@@ -58,7 +58,7 @@ class index:
     <div id="export-dialog" style="display: none;">
       <p id="exportText"></p>
     </div>
-    <div id="scheme-status" style="visibility: hidden; text-align: center; margin-top: 30px; margin-left: auto; margin-right: auto; font-family: helvetica, sans-serif;">Switching schemes.  Please be patient...</div>
+    <div id="scheme-status" style="visibility: hidden; text-align: center; margin-top: 30px; margin-left: auto; margin-right: auto; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif;">Switching schemes.  Please be patient...</div>
     <div id="dialog" title="New note" style="display: none;">
       <p id="track-title" style="text-align: center; font-weight: bold;"></p>
       <p id="date"></p>
@@ -74,7 +74,7 @@ class index:
         <textarea id="textOfNote" cols=60 rows=7 name="textOfNote" class="text ui-widget-content ui-corner-all"></textarea> 
       </form> 
     </div>
-    <div id="lab-key" style="display: none; padding: 5px; position: absolute; top:123px; right: 30px; font-family: Lucida Grande, sans-serif; font-size: 12px; color: #6D869F; border-style: solid; border-width: 1px; border-radius: 5px; -moz-border-radius: 5px; border-color: grey;">
+    <div id="lab-key" style="display: none; padding: 5px; position: absolute; top:123px; right: 30px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helvetica, sans-serif; font-size: 12px; color: #6D869F; border-style: solid; border-width: 1px; border-radius: 5px; -moz-border-radius: 5px; border-color: grey;">
       Lab Key
       <table>
         <tr>
